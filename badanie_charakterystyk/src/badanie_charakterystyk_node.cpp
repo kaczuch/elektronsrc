7ca4feb3b6a807c6de5d7840ff263a291af831a7
Bartosz Kaczor <bkaczor@starogardzka.(none)>
2014-08-06 19:17:26 +0200

    poczatek badan

---
+++ badanie_charakterystyk/src/badanie_charakterystyk_node.cpp
@@ -0,0 +1,101 @@
+#include <termios.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+class badanie {
+private:
+	geometry_msgs::Twist vel_;
+	ros::NodeHandle nh_;
+	double l_scale_, a_scale_,procent;
+	ros::Publisher vel_pub_;
+public:
+	void init() {
+		nh_.param("scale_angular", a_scale_, 1.0);
+                nh_.param("scale_linear", l_scale_, 0.23);
+                vel_pub_ = nh_.advertise<geometry_msgs::Twist> ("cmd_vel", 1);
+
+                ros::NodeHandle n_private("~");
+        }
+
+        void keyboardLoop();
+
+
+};
+int kfd = 0;
+struct termios cooked, raw;
+
+void quit(int sig) {
+        tcsetattr(kfd, TCSANOW, &cooked);
+        exit(0);
+}
+
+int main(int argc, char** argv) {
+
+        ros::init(argc, argv, "elektron_teleop_keyboard");
+        puts("moving robot set parameter to percent of PWM");
+        puts("------set-PWM-value---------------------");
+	std::cin>>procent
+        ElektronTeleopKeyboard tpk;
+        tpk.init();
+
+        signal(SIGINT, quit);
+
+        tpk.keyboardLoop();
+
+        return (0);
+}
+void ElektronTeleopKeyboard::keyboardLoop() {
+        char c;
+        bool dirty = false;
+
+        // get the console in raw mode
+        tcgetattr(kfd, &cooked);
+        memcpy(&raw, &cooked, sizeof(struct termios));
+        raw.c_lflag &= ~(ICANON | ECHO);
+        // Setting a new line, then end of file
+        raw.c_cc[VEOL] = 1;
+        raw.c_cc[VEOF] = 2;
+        tcsetattr(kfd, TCSANOW, &raw);
+
+        puts("moving robot set parameter to percent of PWM");
+        puts("---------------------------");
+
+
+	vel_.linear.x = 0;
+	vel_angular.z=0;
+	for(;;) {
+
+               // dirty = false;
+                // get the next event from the keyboard
+                if (read(kfd, &c, 1) < 0) {
+                        perror("read():");
+                        exit(-1);
+                }
+
+                
+                
+
+                switch (c) {
+                
+                case KEYCODE_SPC:
+                        vel_.linear.x = 0;
+                        vel_.angular.z = 0;
+                        dirty = true;
+                //      std::cout << "SPACE"<<std::endl;
+                        break;
+                }
+
+ 
+		vel_pub_.publish(vel_
+
+
+
+                                                                                      97,0-1        94%
+        }
+}
+                                                                                        157,1         Dół
+
+
